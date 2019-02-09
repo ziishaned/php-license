3532f129ee7f789b2ced02979dce01c5c6f8874c
Zeeshan Ahmad <ziishaned@gmail.com>
2019-02-09 16:25:19 +0400

    Add more tests

--- tests/PhpLicenseTest.php
+++ tests/PhpLicenseTest.php
@@ -16,7 +16,40 @@ class PhpLicenseTest extends TestCase
     /**
      * @throws \Exception
      */
-    public function testCanGetLicense()
+    public function testCanGenerateLicenseKey()
+    {
+        $data       = [
+            "email" => "ziishaned@gmail.com",
+        ];
+        $privateKey = file_get_contents(__DIR__ . '/keys/private_key.pem');
+
+        $license = PhpLicense::generate($data, $privateKey);
+
+        $this->assertIsString($license);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanThrowExceptionForWrongPublicKey()
+    {
+        $data       = [
+            "email" => "ziishaned@gmail.com",
+        ];
+        $privateKey = '';
+
+        try {
+            PhpLicense::generate($data, $privateKey);
+        } catch (\Exception $e) {
+            $this->assertIsString($e->getMessage());
+            $this->assertEquals($e->getMessage(), 'OpenSSL: Unable to get private key');
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanParseLicenseKey()
     {
         $data       = [
             "email" => "ziishaned@gmail.com",
@@ -29,5 +62,18 @@ class PhpLicenseTest extends TestCase
 
         $this->assertIsString($license);
         $this->assertIsArray($parsedLicense);
+    }
+
+    public function testCanThrowExceptionForWrongPrivateKey()
+    {
+        $licenseKey = 'license-key';
+        $privateKey = '';
+
+        try {
+            PhpLicense::parse($licenseKey, $privateKey);
+        } catch (\Exception $e) {
+            $this->assertIsString($e->getMessage());
+            $this->assertEquals($e->getMessage(), 'OpenSSL: Unable to get public key');
+        }
     }
 }
